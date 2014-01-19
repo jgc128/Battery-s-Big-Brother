9a866010136ec80f67ab975d9eb6df168d822d78
Alexey Romanov <jgc128@outlook.com>
2014-01-19 14:39:34 +0400

    first commit

---
+++ Battery's Big Brother - WP/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using Battery_s_Big_Brother___WP.Resources;
+
+namespace Battery_s_Big_Brother___WP
+{
+	/// <summary>
+	/// Provides access to string resources.
+	/// </summary>
+	public class LocalizedStrings
+	{
+		private static AppResources _localizedResources = new AppResources();
+
+		public AppResources LocalizedResources { get { return _localizedResources; } }
+	}
+}
